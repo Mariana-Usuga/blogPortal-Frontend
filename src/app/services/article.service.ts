4558915254343a8c9c0c63f99899beec26a694ec
Mariana-Usuga <marianausugamontoya12344@gmail.com>
2023-09-21 12:58:53 -0500

    add

--- src/app/services/article.service.ts
+++ src/app/services/article.service.ts
@@ -8,14 +8,11 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class ArticleService {
-  //private apiUrl = 'http://localhost:8080/api/article'; // Reemplaza con la URL de tu API
-  //apiUrl = process?.env['API_URL'];
-  apiUrl = environment.apiUrl; // Utiliza environment.apiUrl
+  apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
   getArticles(): Observable<article[]>{
-    console.log('this', `${this.apiUrl}/api/article`)
     return this.http.get<article[]>(`${this.apiUrl}/api/article`);
   }
 
