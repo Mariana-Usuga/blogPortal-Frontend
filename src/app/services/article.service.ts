995331eb721b66849f1939e3377fa63bf2d78efb
Mariana-Usuga <marianausugamontoya12344@gmail.com>
2023-09-21 11:53:20 -0500

    fix

--- src/app/services/article.service.ts
+++ src/app/services/article.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { article } from '../model/article.inferces';
-import { environment } from 'src/environments/environment.prod';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
